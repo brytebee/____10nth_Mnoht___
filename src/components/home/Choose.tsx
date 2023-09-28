e3f5e5eb97714b3538f749a5bc14b0bf0ade684c
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-09-28 16:09:43 +0100

    feat(app): Complete why choose us

--- src/components/home/Choose.tsx
+++ src/components/home/Choose.tsx
@@ -26,7 +26,7 @@ export default function WhyChooseUsSection({ }: Props) {
       <div className='w-11/12 mx-auto lg:w-11/12 md:w-full md:px-2 lg:px-0'>
         <h3 className='text-2xl md:text-3xl md:font-bold font-semibold mb-8'>Why Choose Us?</h3>
         <p className='text-base font-light text-justify md:hidden lg:hidden'>We offer a wide range of courses designed to equip you with the skills needed to succeed in the tech industry. Here are some of the reasons why you should choose us.</p>
-        <div className='md:grid md:grid-cols-2 md:gap-10'>
+        <div className='md:grid md:grid-cols-2 md:gap-3'>
         {reasons.map(({ reason, description }, index) => (
           <div key={index} className='flex gap-[6px] mt-8 md:mt-0 lg:w-[600px] md:w-[400px]'>
             <div className='h-[10px] w-[10px] rounded-full bg-white mt-2' />
