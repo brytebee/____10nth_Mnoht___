6051f28cde2f2da57eab9c2f9163b2633ae0b69a
Atsighi Bright <brytebee@gmail.com>
2023-09-28 12:20:09 +0100

    Added more assets and basic styling

--- src/app/course/[details]/page.tsx
+++ src/app/course/[details]/page.tsx
@@ -4,8 +4,10 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import Back from "../../../../public/svgs/back-icon.svg";
 import Image from "next/image";
+import UIUX from "../../../../public/svgs/uiux.svg";
 
 interface Course {
+  image: String | StaticImport;
   name: String;
   description: String;
   eligibility: String;
@@ -23,6 +25,7 @@ const page = () => {
   // const courseDetails = fetchCourseDetailsById(id);
   const courses: Course[] = [
     {
+      image: UIUX,
       name: "UIUX DESIGN",
       description:
         "This course is designed for individuals who aspire to enter the creative and design industry as designers. It is tailored for those who wish to develop and present their creative ideas for software and computerized devices, addressing real-life interface design challenges in a simple and innovative manner. The course also focuses on acquiring practical expertise in industry-standard software such as Figma and Adobe XD, enabling participants to match the skills of other professionals in the field.",
@@ -44,30 +47,63 @@ const page = () => {
   };
 
   return (
-    <div>
-      <button onClick={handleClick}>
-        <Image width={38} height={38} src={Back} alt="go back" />
+    <div className="text-left px-[26px] pb-[62px]">
+      <button className="p-0" onClick={handleClick}>
+        <Image
+          className="p-0"
+          width={38}
+          height={38}
+          src={Back}
+          alt="go back"
+        />
       </button>
       {courses &&
         courses.map(
-          ({ name, description, eligibility, learnings, conclusion }) => (
-            <>
-              <h1>{name.toUpperCase()}</h1>
-              <p>{description}</p>
-              <h3>Program Eligibility</h3>
-              <p>{eligibility}</p>
-              <h3>What you’ll learn</h3>
-              <p>{learnings.intro}</p>
-              <ul className="list">
+          ({
+            image,
+            name,
+            description,
+            eligibility,
+            learnings,
+            conclusion,
+          }) => (
+            <div>
+              <div className="flex justify-center items-center">
+                <Image
+                  width={280}
+                  height={280}
+                  src={image}
+                  alt={`${name} image`}
+                />
+              </div>
+              <h1 className="text-xl pt-[30px] font-bold">
+                {name.toUpperCase()}
+              </h1>
+              <p className="leading-7 pt-[10px]">{description}</p>
+              <h3 className="pt-[28px] text-lg font-semibold">
+                Program Eligibility
+              </h3>
+              <p className="leading-7 pt-[10px]">{eligibility}</p>
+              <h3 className="pt-[28px] text-lg font-semibold">
+                What you’ll learn
+              </h3>
+              <p className="leading-7 pt-[10px]">{learnings.intro}</p>
+              <ul className="pt-[18px] list-disc list-inside pl-1">
                 {learnings.listings.split(",").map((list) => (
-                  <li key={list}>{list}</li>
+                  <li className="leading-7 pt-[2px] font-semibold" key={list}>
+                    {list}
+                  </li>
                 ))}
               </ul>
-              <p>{conclusion}</p>
-            </>
+              <p className="pt-[20px] leading-7">{conclusion}</p>
+            </div>
           )
         )}
-      <button>Enroll now</button>
+      <div className="text-center pt-[52px]">
+        <button className="w-[150px] py-[10px] px-[24px] justify-center text-center bg-[#D97508] text-white text-primary text-lg rounded-[10px]">
+          Enroll now
+        </button>
+      </div>
     </div>
   );
 };
