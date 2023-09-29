4263b2562a8d0bbec0f2bbeae0ca6b4f93b65774
Atsighi Bright <brytebee@gmail.com>
2023-09-29 07:18:08 +0100

    Added static data

---
+++ src/app/staticData/index.ts
@@ -0,0 +1,9 @@
+import UIUX from "../../../public/svgs/uiux.svg";
+import DEV from "../../../public/svgs/dev.svg";
+import MOBDEV from "../../../public/svgs/mob_dev.svg";
+
+type Img = HTMLImageElement;
+
+const Images: Img[] = [UIUX, DEV, MOBDEV];
+
+export default Images;
