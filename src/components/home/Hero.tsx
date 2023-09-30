840c7efe248eca1d71ce580957fa27582b2f9a94
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-09-30 13:59:40 +0100

    refactor(app): Refactor hero_bg image
    Adjust the top on tablets & alignment for hero text on desktop

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import HeroImage from '../../../public/svgs/hero-img.svg'
-import DesktopHeroBg from '../../../public/svgs/grouped-hero-bg.svg'
+import DesktopHeroBg from '../../../public/svgs/hero_bg.svg'
 
 type Props = {}
 
@@ -25,10 +25,10 @@ export default function Hero({ }: Props) {
       </section>
       <section className='hidden md:flex lg:flex relative lg:w-full'>
         <Image src={DesktopHeroBg} alt='Desktop Hero bg' className='object-cover w-full h-full' />
-        <div className='absolute lg:top-[120px] md:top-6 md:left-4 lg:left-12'>
+        <div className='absolute lg:top-[120px] md:top-12 md:left-4 lg:left-12'>
           <div className='flex flex-col lg:gap-4 md:gap-2'>
             <h1 className='text-white md:text-2xl lg:text-4xl font-semibold md:w-[20rem] lg:w-[29rem]'>Online Tech Classes From The Best Instructors</h1>
-            <div className='flex flex-col lg:gap-2 md:gap-1 md:pl-4 lg:pl-8'>
+            <div className='flex flex-col lg:gap-2 md:gap-1 md:pl-4 lg:pl-6'>
               <h2 className='text-primary text-xl font-semibold'>Introduction</h2>
               <p className='lg:w-[650px] md:w-[350px] lg:text-lg md:text-xs md:text-justify lg:text-left font-normal text-white'>Welcome to TechVerse Academy, your gateway to unlocking the world of cutting-edge technology! Whether you're a curious beginner or an aspiring tech enthusiast, our online platform offers an unparalleled learning experience tailored to your needs.</p>
               <div className='lg:mt-8 md:mt-2 flex gap-5'>
