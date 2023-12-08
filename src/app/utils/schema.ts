ec38c48edfb5bba53f639f225e8f72437f298086
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-12-08 15:18:20 +0100

    feat(app): Validate forms and conditional set border color

--- src/app/utils/schema.ts
+++ src/app/utils/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const contactSchema = z.object({
-  name: z.string().min(3, { message: 'Name Username must be at least 3 characters' }).max(50, { message: 'Name cannot be more than 50 characters' }),
+  name: z.string().min(3, { message: 'Name must be at least 3 characters' }).max(50, { message: 'Name cannot be more than 50 characters' }),
   email: z.string().min(1, { message: 'Email is required' }).email('Invalid email address'),
   message: z.string().min(3, { message: 'Message must be minimum 3 characters' })
 })
