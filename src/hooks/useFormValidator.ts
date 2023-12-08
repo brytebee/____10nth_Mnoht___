9eb2bc64e5a7a665a7dd4e743c3ee7dbe1a72b41
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-12-08 15:17:25 +0100

    feat(app): Add resolver to hook

--- src/hooks/useFormValidator.ts
+++ src/hooks/useFormValidator.ts
@@ -1,29 +1,24 @@
-import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { ZodError, ZodObject, ZodRawShape } from "zod";
+import { ZodObject, ZodRawShape } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export const useFormWithValidation = <TFormData extends ZodRawShape>(validationSchema: ZodObject<TFormData>) => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<TFormData>();
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<TFormData>({
+    resolver: zodResolver(validationSchema),
+  });
 
   const onSubmit: SubmitHandler<TFormData> = async (data) => {
-    try {
-      await validationSchema.parseAsync(data);
-      console.log("Form data is valid:", data);
-    } catch (error) {
-      if (error instanceof ZodError) {
-        const formattedErrors = error.errors.map((e) => ({
-          field: e.path.join("."),
-          message: e.message,
-        }));
-        return error
-        console.error("Validation errors:", formattedErrors);
-      }
-    }
+    // simulate API call
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+    // send toast notification here
+    reset();
   };
 
-  return { register, handleSubmit, onSubmit, errors };
+  return { register, handleSubmit, onSubmit, errors, isSubmitting };
 };
