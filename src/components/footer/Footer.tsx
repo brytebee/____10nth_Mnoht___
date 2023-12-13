16824a9fbfb45a1c2bfd42b0672ca167ed596151
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-12-13 15:03:32 +0100

    feat(app): Add social Icons to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,10 @@
-'use client';
-
 import React from "react";
 import Image from 'next/image'
 import TechverseLogo from '../../../public/svgs/techverse-logo.svg'
 import Links from "./Links";
-import useMediaQuery from "@/hooks/useMediaQuery";
+import FooterSocialIcons from "./FooterSocialIcons";
 
 const Footer = () => {
-  const isMobile = useMediaQuery("(min-width: 900px)");
   const footerHeaders = [
     {
       title: "Company", links: [
@@ -42,12 +39,15 @@ const Footer = () => {
             <p className="text-white text-[12px] leading-4 font-normal absolute top-14">where knowledge meets innovation</p>
           </div>
 
-          <div className="flex flex-col gap-[3px] w-full rounded-md relative md:w-[280px] lg:w-[420px]">
-            <h4 className="text-white text-[14px] leading-5 font-medium lg:hidden">Sign up for our newsletter</h4>
-            <div>
-              <input type="email" name="email" placeholder="Sign up for our newsletter" className="w-full py-3 px-2 md:pl-1 lg:px-2 rounded-md" />
-              <button type="button" className="bg-primary text-white text-sm w-[111px] md:w-[100px] py-3 px-3 md:px-1 lg:px-1 lg:w-[100px] rounded-md shadow-[0px 7px 52px 0px] absolute right-1 bottom-0.5 font-bold">{isMobile ? 'Sign up' : 'Sign up'}</button>
+          <div>
+            <div className="flex flex-col gap-[3px] w-full rounded-md relative md:w-[280px] lg:w-[420px]">
+              <h4 className="text-white text-[14px] leading-5 font-medium lg:hidden">Sign up for our newsletter</h4>
+              <div>
+                <input type="email" name="email" placeholder="Sign up for our newsletter" className="w-full py-3 px-2 md:pl-1 lg:px-2 rounded-md" />
+                <button type="button" className="bg-primary text-white text-sm w-[90px] md:w-[100px] py-3 px-3 md:px-1 lg:px-1 lg:w-[100px] rounded-md shadow-[0px 7px 52px 0px] absolute right-1 bottom-0.5 font-bold">Sign up</button>
+              </div>
             </div>
+            <FooterSocialIcons />
           </div>
         </section>
 
