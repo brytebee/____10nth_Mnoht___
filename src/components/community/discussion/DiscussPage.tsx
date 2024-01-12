d855cbffcfb33deb839e8869aa999700773f1502
Bright Atsighi <brytebee@gmail.com>
2024-01-12 10:12:15 +0100

    Style changes on header for discussionnn details for PC

--- src/components/community/discussion/DiscussPage.tsx
+++ src/components/community/discussion/DiscussPage.tsx
@@ -4,7 +4,7 @@ import Card from '@/components/community/discussion/Card';
 
 const DiscussPage = () => (
   <section className="text-center lg:px-12 py-5">
-    <h2 className="text-left pl-[28px] text-[1.6rem] lg:text-[2.2rem] font-bold leading-9 lg:leading-[41px]">
+    <h2 className="text-left pl-[28px] lg:pl-16 text-[1.6rem] lg:text-[2.2rem] font-bold leading-9 lg:leading-[41px] lg:w-4/12">
       Top Stories From Our Students
     </h2>
     <section className="lg:grid lg:grid-cols-3 lg:gap-2 lg:mx-8">
