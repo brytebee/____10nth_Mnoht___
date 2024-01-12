6d955482a0aefb59de727beeb08e741c8a6347a6
Bright Atsighi <brytebee@gmail.com>
2024-01-12 07:08:41 +0100

    Fixed PC discussion card header

--- src/components/community/discussion/Header.tsx
+++ src/components/community/discussion/Header.tsx
@@ -10,9 +10,9 @@ type Props = {
 
 const Header = ({ image, title, username }: Props) => {
   return (
-    <header className="flex flex-col items-center lg:flex lg:flex-row lg:w-full lg:justify-around">
+    <header className="flex flex-col items-center lg:flex lg:flex-row lg:w-full">
       <Image className="w-12" src={image} alt={`${title} photo`} />
-      <div className="flex flex-col-reverse lg:items-start">
+      <div className="flex flex-col-reverse lg:items-start ml-5">
         <h3 className="font-normal text-lg pb-4">{title}</h3>
         <Image src={Line} alt="line" />
         <p className="text-sm leading-7">{username}</p>
