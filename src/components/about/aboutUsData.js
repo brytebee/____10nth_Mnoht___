d9c200193d87181a901719ab5fceaf5840637e64
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-01-12 14:59:45 +0100

    refactor(app): Update team photo
    Replace old photo with new one

--- src/components/about/aboutUsData.js
+++ src/components/about/aboutUsData.js
@@ -1,5 +1,5 @@
 import anny from '../../../public/svgs/anny.svg';
-import ola from '../../../public/images/ola.jpg';
+import ola from '../../../public/images/ola.jpeg';
 import okey from '../../../public/images/okey.png';
 import Bright from '../../../public/svgs/Bright.svg';
 import ab from '../../../public/svgs/ab.svg';
@@ -55,54 +55,55 @@ const team = [
 ];
 
 const team2 = [{
- id: 1,
-    img: ola,
-    name: 'Oladipupo Ishola',
-    designation: 'Senior full-stack developer',
-    instaLink: 'https://www.instagram.com/olaishola05/',
-    twitterLink: 'https://twitter.com/olaishola05?t=1v6gVd9yOPNOhXvwHbKLSA&s=09',
-    linkedInLink: 'https://www.linkedin.com/in/olaishola05',
-    facebookLink: 'https://www.facebook.com/olaishola05?mibextid=ZbWKwL',
-    insta,
-    twitter,
-    linkedIn,
-    facebook,
+  id: 1,
+  img: ola,
+  name: 'Oladipupo Ishola',
+  designation: 'Senior full-stack developer',
+  instaLink: 'https://www.instagram.com/olaishola05/',
+  twitterLink: 'https://twitter.com/olaishola05?t=1v6gVd9yOPNOhXvwHbKLSA&s=09',
+  linkedInLink: 'https://www.linkedin.com/in/olaishola05',
+  facebookLink: 'https://www.facebook.com/olaishola05?mibextid=ZbWKwL',
+  insta,
+  twitter,
+  linkedIn,
+  facebook,
 },
 {
-    id: 2,
-    img: okey,
-    name: 'Oke Oluwasegun',
-    designation: 'Product Manager',
-    instaLink: 'https://www.instagram.com/oluwasegunoke/',
-    twitterLink: 'https://twitter.com/oluwasegunoke?t=1v6gVd9yOPNOhXvwHbKLSA&s=09',
-    linkedInLink: 'https://www.linkedin.com/in/oluwasegunoke',
-    facebookLink: 'https://www.facebook.com/oluwasegunoke?mibextid=ZbWKwL',
-    insta,
-    twitter,
-    linkedIn,
-    facebook,}];
+  id: 2,
+  img: okey,
+  name: 'Oke Oluwasegun',
+  designation: 'Product Manager',
+  instaLink: 'https://www.instagram.com/oluwasegunoke/',
+  twitterLink: 'https://twitter.com/oluwasegunoke?t=1v6gVd9yOPNOhXvwHbKLSA&s=09',
+  linkedInLink: 'https://www.linkedin.com/in/oluwasegunoke',
+  facebookLink: 'https://www.facebook.com/oluwasegunoke?mibextid=ZbWKwL',
+  insta,
+  twitter,
+  linkedIn,
+  facebook,
+}];
 
-    const chooseUsData = [
-     {
-     id: 1,
-     bullet: 'Small Class Sizes:',
-     text: 'We keep our class sizes small to ensure that you receive personalized attention and support from your instructors.',
-    },
-     {
-     id: 2,
-     bullet: 'Strong Industry Connections:',
-     text: 'We have established partnerships with leading tech companies and organizations to provide you with networking opportunities, job placement assistance, and real-world projects.',
-    },
-     {
-     id: 3,
-     bullet: 'Diverse Community:',
-     text: 'We welcome students from all backgrounds, fostering a diverse and inclusive learning environment that encourages collaboration and innovation.',
-    },
-     {
-     id: 4,
-     bullet: 'Career Support:',
-     text: 'Our career services team is dedicated to helping you achieve your professional goals. From resume building to interview preparation, we are here to support your career journey.',
-    },
-   ]
+const chooseUsData = [
+  {
+    id: 1,
+    bullet: 'Small Class Sizes:',
+    text: 'We keep our class sizes small to ensure that you receive personalized attention and support from your instructors.',
+  },
+  {
+    id: 2,
+    bullet: 'Strong Industry Connections:',
+    text: 'We have established partnerships with leading tech companies and organizations to provide you with networking opportunities, job placement assistance, and real-world projects.',
+  },
+  {
+    id: 3,
+    bullet: 'Diverse Community:',
+    text: 'We welcome students from all backgrounds, fostering a diverse and inclusive learning environment that encourages collaboration and innovation.',
+  },
+  {
+    id: 4,
+    bullet: 'Career Support:',
+    text: 'Our career services team is dedicated to helping you achieve your professional goals. From resume building to interview preparation, we are here to support your career journey.',
+  },
+]
 
 export { team, team2, chooseUsData };
