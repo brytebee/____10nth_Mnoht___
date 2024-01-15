6f3393d1e63121fe06e1f600d81470109c4708b8
Bright Atsighi <brytebee@gmail.com>
2024-01-15 14:32:26 +0100

    Updated metrics with + to signify dynamism

--- src/components/community/metrics/Metrics.tsx
+++ src/components/community/metrics/Metrics.tsx
@@ -12,19 +12,19 @@ const Metrics = ({ course, student, instructor, certified }: Props) => {
     <div className="flex justify-around py-[146px]">
       <div className="course text-center flex flex-col">
         <span className="text-2xl font-bold">Courses</span>
-        <span className="text-3xl font-black pt-4">{course}</span>
+        <span className="text-3xl font-black pt-4">{course}+</span>
       </div>
       <div className="student text-center flex flex-col">
         <span className="text-2xl font-bold">Students enrolled</span>
-        <span className="text-3xl font-black pt-4">{student}</span>
+        <span className="text-3xl font-black pt-4">{student}+</span>
       </div>
       <div className="instructor text-center flex flex-col">
         <span className="text-2xl font-bold">Instructors</span>
-        <span className="text-3xl font-black pt-4">{instructor}</span>
+        <span className="text-3xl font-black pt-4">{instructor}+</span>
       </div>
       <div className="certified text-center flex flex-col">
         <span className="text-2xl font-bold">Certified students</span>
-        <span className="text-3xl font-black pt-4">{certified}</span>
+        <span className="text-3xl font-black pt-4">{certified}+</span>
       </div>
     </div>
   );
